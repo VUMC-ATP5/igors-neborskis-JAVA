3216f0eb17fa69813d19c8aa83ad7c4bb2b8d3dd
igors.neborskis <igors.neborskis@gmail.com>
2022-09-26 22:16:36 +0300

    Initial commit

---
+++ src/main/java/lekcijaViens/labDarbs/main.java
@@ -0,0 +1,14 @@
+package lekcijaViens.labDarbs;
+
+public class main {
+    public static void main(String[] args) {
+        //int - integer
+        int vecums = 39;
+        System.out.println("Mans vecums ir " + vecums + " gadi.");
+        int augums = 182;
+        System.out.println("Mans augums ir " + augums + "cm.");
+        String acuKrasa = "zaļa";
+        System.out.println("Mana acu krāsa ir " + acuKrasa + ".");
+
+    }
+}
