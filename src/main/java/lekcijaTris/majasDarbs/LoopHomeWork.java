958a59f3d87ea4fadbf63df656ef15b442778973
igors.neborskis <igors.neborskis@gmail.com>
2022-10-12 22:22:58 +0300

    Update LoopHomeWork.java

--- src/main/java/lekcijaTris/majasDarbs/LoopHomeWork.java
+++ src/main/java/lekcijaTris/majasDarbs/LoopHomeWork.java
@@ -2,31 +2,31 @@ package lekcijaTris.majasDarbs;
 
 public class LoopHomeWork {
     public static void main(String[] args) {
-//        //NewLine.newLine();
-//        UzdevumsViens.uzdevumsViens();
-//        PromptEnterKey.promptEnterKey();
-//
-//        //NewLine.newLine();
-//        UzdevumsDivi.uzdevumsDivi();
-//        PromptEnterKey.promptEnterKey();
-//
-//        //NewLine.newLine();
-//        UzdevumsTris.uzdevumsTris();
-//
-//        //NewLine.newLine();
-//        UzdevumsCetri.uzdevumsCetri(3);
-//        UzdevumsCetri.uzdevumsCetri(4);
-//        UzdevumsCetri.uzdevumsCetri(5);
-//        UzdevumsCetri.uzdevumsCetri(15);
-//        UzdevumsCetri.uzdevumsCetri(25);
-//        UzdevumsCetri.uzdevumsCetri(50);
-//        PromptEnterKey.promptEnterKey();
-//
-//        //NewLine.newLine();
+        //NewLine.newLine();
+        UzdevumsViens.uzdevumsViens();
+        PromptEnterKey.promptEnterKey();
+
+        //NewLine.newLine();
+        UzdevumsDivi.uzdevumsDivi();
+        PromptEnterKey.promptEnterKey();
+
+        //NewLine.newLine();
+        UzdevumsTris.uzdevumsTris();
+
+        //NewLine.newLine();
+        UzdevumsCetri.uzdevumsCetri(3);
+        UzdevumsCetri.uzdevumsCetri(4);
+        UzdevumsCetri.uzdevumsCetri(5);
+        UzdevumsCetri.uzdevumsCetri(15);
+        UzdevumsCetri.uzdevumsCetri(25);
+        UzdevumsCetri.uzdevumsCetri(50);
+        PromptEnterKey.promptEnterKey();
+
+        //NewLine.newLine();
         ExtraUzdevumsCetri.extraUzdevumsCetri();
         PromptEnterKey.promptEnterKey();
 
-//        //NewLine.newLine();
-//        ExtraUzdevumsPieci.extraUzdevumsPieci();
+        //NewLine.newLine();
+        ExtraUzdevumsPieci.extraUzdevumsPieci();
     }
 }
